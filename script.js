2b670738c5fccfb806d22d4d7fcade20bb55bbeb
Ayan Sahil <ayans8557@gmail.com>
2026-01-03 19:39:42 +0530

    todoList ✅ version1

--- script.js
+++ script.js
@@ -15,15 +15,65 @@ function goBack() {
     });
   });
 }
-
 goBack();
 
-let form = document.querySelector(".addTask form");
-let taskInput = document.querySelector(".addTask form input");
-let taskDetailsInput = document.querySelector(".addTask form textarea");
+function todoList() {
+  let currentTask = [];
 
+  if (localStorage.getItem("currentTask")) {
+    currentTask = JSON.parse(localStorage.getItem("currentTask"));
+  }
 
+  function renderTask() {
+    let allTask = document.querySelector(".allTask");
+    let sum = "";
 
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-});
+    currentTask.forEach(function (elem, idx) {
+      sum += `
+        <div class="task">
+          <h5>${elem.task} <span class=${elem.important}>imp</span></h5>
+          <button id=${idx}>Mark as Completed</button>
+        </div>
+      `;
+    });
+
+    // ✅ pehle HTML render
+    allTask.innerHTML = sum;
+
+    // ✅ fir localStorage update
+    localStorage.setItem("currentTask", JSON.stringify(currentTask));
+
+    // ✅ fir button events
+    document.querySelectorAll(".task button").forEach(function (btn) {
+      btn.addEventListener("click", function () {
+        currentTask.splice(btn.id, 1);
+        renderTask();
+      });
+    });
+  }
+
+  renderTask();
+
+  let form = document.querySelector(".addTask form");
+  let taskInput = document.querySelector(".addTask form #task-inp");
+  let taskDetailsInput = document.querySelector(".addTask form textarea");
+  let taskCheckbox = document.querySelector(".addTask form #check");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    currentTask.push({
+      task: taskInput.value,
+      details: taskDetailsInput.value,
+      important: taskCheckbox.checked,
+    });
+
+    renderTask();
+
+    taskCheckbox.checked = false;
+    taskInput.value = "";
+    taskDetailsInput.value = "";
+  });
+}
+
+todoList();
